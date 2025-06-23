4d82d3e99ba0fc135d8fc61bd56b7a8324f9fb5c
Shivam <thisisshivamkumar8840@gmail.com>
2025-06-23 22:57:45 +0530

    headerandblog

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,13 +1,24 @@
 import React from "react";
 import { assets } from "../assets/assets";
+import { useNavigate } from "react-router-dom";
 
 const Navbar = () => {
+  const navigate = useNavigate();
+
+
+
   return (
     <div className="flex justify-between items-center py-5 mx-8 sm:mx-20 xl:mx-32">
-      <img src={assets.logo} alt="Brand Logo" className="w-32 sm:w-44 cursor-pointer" />
+      <img
+        onClick={() => navigate("/")}
+        src={assets.logo}
+        alt="Brand Logo"
+        className="w-32 sm:w-44 cursor-pointer"
+      />
 
       <button
-        className="flex items-center gap-2 bg-blue-600 text-white px-4 py-2 rounded-full hover:bg-blue-700 transition duration-200"
+        onClick={() => navigate("/admin")}
+        className="flex items-center gap-2 bg-blue-600 text-white px-4 py-2 rounded-full hover:bg-green-700 transition duration-200 cursor-pointer"
         aria-label="Login to your account"
       >
         <span>Login</span>
