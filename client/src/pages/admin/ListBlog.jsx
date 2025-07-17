4caf3cad1f7a954b6bad5fc49e51556a21048143
Shivam <thisisshivamkumar8840@gmail.com>
2025-07-17 19:15:24 +0530

    .

--- client/src/pages/admin/ListBlog.jsx
+++ client/src/pages/admin/ListBlog.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { blog_data } from '../../assets/assets';
+import BlogTablesItems from './BlogTablesItems';
 
 const ListBlog = () => {
-  return (
-    <div>ListBlog</div>
-  )
-}
+  const [blogs, setBlogs] = useState([]);
 
-export default ListBlog
+  const fetchBlogs = async () => {
+    setBlogs(blog_data);
+  };
+
+  useEffect(() => {
+    fetchBlogs();
+  }, []);
+
+  return (
+    <div className="flex-1 pt-5 px-5 sm:pt-12 sm:pl-16 bg-blue-50/50">
+      <h1>All Blogs</h1>
+
+      <div className="relative h-4/5 mt-4 max-w-4xl overflow-x-auto shadow rounded-lg scrollbar-hide bg-white">
+        <table className="w-full text-sm text-gray-500">
+          <thead className="text-xs text-gray-600 text-left uppercase">
+            <tr>
+              <th className="px-2 py-4 xl:px-6">#</th>
+              <th className="px-2 py-4">Blog title</th>
+              <th className="px-2 py-4 max-sm:hidden">Date</th>
+              <th className="px-2 py-4 max-sm:hidden">Status</th>
+              <th className="px-2 py-4">Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {blogs.map((blog, index) => (
+              <BlogTablesItems key={index} blog={blog} index={index} />
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
+
+export default ListBlog;
